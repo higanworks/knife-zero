12d1d4f4c953ffca5eeb570a2a297d1c9885ad63
sawanoboly <sawanoboriyu@higanworks.com>
2015-03-04 19:49:27 +0900

    add simplecov

--- knife-zero.gemspec
+++ knife-zero.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "test-unit"
+  spec.add_development_dependency "simplecov"
 
   spec.add_runtime_dependency "chef"
 end
--- test/run_test.rb
+++ test/run_test.rb
@@ -5,4 +5,12 @@ test_dir = File.join(base_dir, "test/unit")
 $LOAD_PATH.unshift(lib_dir)
 
 require 'test/unit'
+require 'simplecov'
+
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+  SimpleCov.start
+end
+
 exit Test::Unit::AutoRunner.run(true, test_dir)
