4fe254e55a7af08bcdad7141853d1ce11b71b1cd
sawanoboly <sawanoboriyu@higanworks.com>
2015-04-07 18:42:18 +0900

    use name for ssh host given attribute

--- integration_test/chef-repo/run.sh
+++ integration_test/chef-repo/run.sh
@@ -4,7 +4,13 @@ set -e
 
 /usr/sbin/sshd -E /tmp/log -o 'LogLevel DEBUG'
 knife zero diagnose
+
+# Use Ipaddress
 knife zero bootstrap 127.0.0.1 -N zerohost -x docker -P docker --sudo -V
 knife node show zerohost
 knife zero chef_client "name:zerohost" -a ipaddress -x docker -P docker --sudo -V
 
+# Use Name
+knife zero bootstrap 127.0.0.1 -N 127.0.0.1 -x docker -P docker --sudo -V
+knife node show 127.0.0.1
+knife zero chef_client "name:127.0.0.1" -a name -x docker -P docker --sudo -V
