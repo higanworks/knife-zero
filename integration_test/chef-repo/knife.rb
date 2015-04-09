24aae882c1e5c1b2ad8ac1debb450f66ca80e8dd
sawanoboly <sawanoboriyu@higanworks.com>
2015-04-09 16:34:31 +0900

    run sample recipes

--- integration_test/chef-repo/knife.rb
+++ integration_test/chef-repo/knife.rb
@@ -1,2 +1,4 @@
 local_mode true
 chef_repo_path "."
+data_bag_encrypt_version 3
+knife[:secret_file] = 'encrypted_data_bag_secret'
--- integration_test/chef-repo/run.sh
+++ integration_test/chef-repo/run.sh
@@ -8,10 +8,10 @@ knife zero diagnose
 # Use Ipaddress
 knife zero bootstrap 127.0.0.1 -N zerohost -x docker -P docker --sudo -V
 knife node show zerohost
-knife zero chef_client "name:zerohost" -a ipaddress -x docker -P docker --sudo -V
+knife zero chef_client "name:zerohost" -a ipaddress -x docker -P docker --sudo -V -o 'kzi::default'
 
 # Use Name
 knife zero bootstrap 127.0.0.1 -N 127.0.0.1 -x docker -P docker --sudo -V
 knife node show 127.0.0.1
 ## Pending until merge https://github.com/chef/chef/pull/3195
-# knife zero chef_client "name:127.0.0.1" -a name -x docker -P docker --sudo -V
+# knife zero chef_client "name:127.0.0.1" -a name -x docker -P docker --sudo -V -o 'kzi::default'
