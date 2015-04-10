21b38c8b7793d5cb89e6c944a91d48865457f442
sawanoboly <sawanoboriyu@higanworks.com>
2015-04-10 16:50:40 +0900

    set -x to run.sh

--- integration_test/chef-repo/run.sh
+++ integration_test/chef-repo/run.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-set -e
+set -xe
 
 /usr/sbin/sshd -E /tmp/log -o 'LogLevel DEBUG'
 knife zero diagnose
 
 # Use Ipaddress
+knife helper exec boot_ipaddress --print
 knife helper exec boot_ipaddress
 knife node show zerohost
 knife helper exec client_ipaddress
+knife helper exec client_ipaddress --print
 
 # Use Name
+knife helper exec boot_name --print
 knife helper exec boot_name
 knife node show 127.0.0.1
 ## Pending until merge https://github.com/chef/chef/pull/3195
