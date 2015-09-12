139e56f5c619fabeb22fb2045e3aa0d176aa8992
sawanoboly <sawanoboriyu@higanworks.com>
2015-09-12 21:55:30 +0900

    add option --json-attribute-file FILE

--- test/knife-zero/test_versioin.rb
+++ test/knife-zero/test_versioin.rb
@@ -2,6 +2,6 @@ require "knife-zero/version"
 
 class TC_Version < Test::Unit::TestCase
   test "returns version correctly" do
-    assert_equal("1.8.2", Knife::Zero::VERSION)
+    assert_equal("1.8.3", Knife::Zero::VERSION)
   end
 end
