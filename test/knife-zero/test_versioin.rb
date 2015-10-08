919226a51ef3d061e622bc2f9dbaebaf634cd88f
sawanoboly <sawanoboriyu@higanworks.com>
2015-10-08 16:58:25 +0900

    pre.1

--- test/knife-zero/test_versioin.rb
+++ test/knife-zero/test_versioin.rb
@@ -2,6 +2,6 @@ require "knife-zero/version"
 
 class TC_Version < Test::Unit::TestCase
   test "returns version correctly" do
-    assert_equal("1.9.0.dev", Knife::Zero::VERSION)
+    assert_equal("1.9.0.pre.1", Knife::Zero::VERSION)
   end
 end
