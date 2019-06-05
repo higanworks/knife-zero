198d614129992cb70340d0820cd6b4cb7b114d36
sawanoboly <sawanoboriyu@higanworks.com>
2019-06-05 16:19:40 +0900

    get ports from current run

--- lib/knife-zero/devpatch/train_connector.rb
+++ lib/knife-zero/devpatch/train_connector.rb
@@ -1,5 +1,5 @@
 require 'chef/knife/bootstrap/train_connector'
-
+require 'knife-zero/helper'
 
 class Chef
   class Knife
@@ -9,7 +9,7 @@ class Chef
           def connect!
             # Force connection to establish
             connection.wait_until_ready
-            connection.instance_variable_get(:@session).forward.remote(8889, '127.0.0.1', 18889)
+            connection.instance_variable_get(:@session).forward.remote(URI.parse(Chef::Config.chef_server_url).port, '127.0.0.1', ::Knife::Zero::Helper.zero_remote_port)
             true
           end
         end
