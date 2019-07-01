d0780310a9a09c9b64dfa2e1293238d4157935d5
sawanoboly <sawanoboriyu@higanworks.com>
2019-07-01 15:07:21 +0900

    remove extra `]` and bump 1.19.6

--- lib/knife-zero/version.rb
+++ lib/knife-zero/version.rb
@@ -1,6 +1,6 @@
 module Knife
   module Zero
-    VERSION = '1.19.5'
+    VERSION = '1.19.6'
     MAJOR, MINOR, TINY = VERSION.split('.')
   end
 end
--- test/knife-zero/test_version.rb
+++ test/knife-zero/test_version.rb
@@ -2,6 +2,6 @@ require "knife-zero/version"
 
 class TC_Version < Test::Unit::TestCase
   test 'returns version correctly' do
-    assert_equal('1.19.5', Knife::Zero::VERSION)
+    assert_equal('1.19.6', Knife::Zero::VERSION)
   end
 end
